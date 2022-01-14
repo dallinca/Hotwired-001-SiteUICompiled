c2ebe39b4c9f4fb36385af7e1423685a4a51edfc
Dallin Andersen <andersen.dallin@gmail.com>
2022-01-13 22:35:39 -0700

    first commit

---
+++ js/contact.8845f7d7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["contact"],{b8fa:function(n,t,c){"use strict";c.r(t);var e=c("7a23"),a=Object(e["g"])("div",{class:"contact component-padding"},[Object(e["g"])("h1",null,"Contact Us"),Object(e["g"])("p",null,"Feel free to contact me at andersen.dallin@gmail.com")],-1),o=[a];function l(n,t){return Object(e["t"])(),Object(e["f"])("section",null,o)}var s=c("6b0d"),d=c.n(s);const i={},r=d()(i,[["render",l]]);t["default"]=r}}]);
+//# sourceMappingURL=contact.8845f7d7.js.map
